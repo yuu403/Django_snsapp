7709e5dc4fd461debff44d1c1e587657af547a93
yuu403 <yyyusuke20@gmail.com>
2026-06-06 21:01:45 +0900

    Fix comment

--- snsapp/admin.py
+++ snsapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post,Connection
+from .models import Post,Follow, Comment
 
 admin.site.register(Post)
-admin.site.register(Connection)
+admin.site.register(Follow)
+admin.site.register(Comment)
 # Register your models here.
